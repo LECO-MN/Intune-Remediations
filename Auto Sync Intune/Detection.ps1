981496d9988de735c5d50c179e25e80982fe2031
LECO-MN <143440812+LECO-MN@users.noreply.github.com>
2023-08-28 14:46:28 -0400

    Create Detection.ps1

---
+++ Auto Sync Intune/Detection.ps1
@@ -0,0 +1,30 @@
+Try {
+    # Get last time PushLaunch scheduled task was executed
+    $PushInfo = Get-ScheduledTask | Where-Object {$_.TaskName -eq 'PushLaunch'} | Get-ScheduledTaskInfo
+    $LastPush = $PushInfo.LastRunTime
+
+    # Get the current datetime
+    $CurrentTime=(GET-DATE)
+
+    $NoTimeDiff = 0
+
+    Try {
+        # Calculate the time difference between the current datetime and the date stored in the variable.
+        $TimeDiff = New-TimeSpan -Start $LastPush -End $CurrentTime
+    } Catch [System.Management.Automation.ParameterBindingException] {
+        $NoTimeDiff = 1
+    }
+
+    # Run the scheduled task!
+    Get-ScheduledTask | Where-Object {$_.TaskName -eq 'PushLaunch'} | Start-ScheduledTask
+
+    If ($NoTimeDiff -eq 0) {
+        Write-Output "Sync Started! Last sync was $($TimeDiff.Hours) hours and $($TimeDiff.Minutes) minutes ago"
+    } Else {
+        Write-Output "Sync Started! The last runtime of the PushLaunch task could not be calculated!"
+    }
+    Exit 0
+} Catch {
+    Write-Error "Err Line: $($_.InvocationInfo.ScriptLineNumber) Err Name: $($_.Exception.GetType().FullName) Err Msg: $($_.Exception.Message)"
+    Exit 1
+}
