9c33f07a5684f19206cb6ec5de1d4e2c4d4c59ec
LECO-MN <143440812+LECO-MN@users.noreply.github.com>
2023-08-28 14:22:20 -0400

    Update Detection.ps1
    Oops... didn't assign Get-CimInstance output to a variable

--- Stale User Profiles/Detection.ps1
+++ Stale User Profiles/Detection.ps1
@@ -1,9 +1,9 @@
 $Days = 60
-Get-CimInstance -ClassName Win32_UserProfile | Where-Object {$_.LastUseTime -lt (get-date).adddays(-$Days)} | Where-Object {$_.Loaded -eq $false}
+$Profiles = Get-CimInstance -ClassName Win32_UserProfile | Where-Object {$_.LastUseTime -lt (get-date).adddays(-$Days)} | Where-Object {$_.Loaded -eq $false}
 If ($Profiles.Count -gt 0) {
-    Write-Output "Stale user profiles detected... remediating" -ForegroundColor Red
+    Write-Output "Stale user profiles detected... remediating"
     Exit 1
 } Else {
-    Write-Output "No stale profiles detected..." -ForegroundColor Green
+    Write-Output "No stale profiles detected..."
     Exit 0
 }
