96a90226e82033c050533793671c642fa40f1877
LECO-MN <143440812+LECO-MN@users.noreply.github.com>
2023-08-28 14:38:40 -0400

    Add some powershell comments

--- Stale User Profiles/Detection.ps1
+++ Stale User Profiles/Detection.ps1
@@ -1,5 +1,9 @@
+# Profiles that havent been logged into after these amounts of days will be deleted
 $Days = 60
+
+# Get list of all profiles older than the $Days variable. Profiles should also not be loaded. 
 $Profiles = Get-CimInstance -ClassName Win32_UserProfile | Where-Object {$_.LastUseTime -lt (get-date).adddays(-$Days)} | Where-Object {$_.Loaded -eq $false}
+
 If ($Profiles.Count -gt 0) {
     Write-Output "Stale user profiles detected... remediating"
     Exit 1
